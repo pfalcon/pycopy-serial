d1b9337954adb388409adf543aeb3be5b5e6b0a6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-06-07 00:41:46 +0300

    Release 0.2.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(name='micropython-serial',
-      version='0.1',
+      version='0.2',
       description="pySerial-like module for MicroPython (unix port).",
       url='https://github.com/pfalcon/micropython-serial',
       author='Paul Sokolovsky',
       author_email='pfalcon@users.sourceforge.net',
       license='MIT',
       py_modules=['serial'],
-      install_requires=['micropython-os'])
+      install_requires=['micropython-os', 'micropython-fcntl'])
