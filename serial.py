ffa88a523b46c60c83cd00d4d9badbe9ebd3b61f
jacob019 <github@jacobstoner.com>
2017-11-22 10:40:57 -0500

    add more baud rates
    micropython's termios does not define the B19200 constant so we use the integer value instead.  The integer value refers to the position in the baud rate table found in the linux kernel's tty_baudrate.c

--- serial.py
+++ serial.py
@@ -23,7 +23,12 @@ class SerialDisconnectException(SerialException):
 
 class Serial:
 
-    BAUD_MAP = { 9600: termios.B9600, 115200: termios.B115200 }
+    BAUD_MAP = {
+        9600: termios.B9600,
+        19200: 14,
+        57600: termios.B57600,
+        115200: termios.B115200
+    }
 
     def __init__(self, port, baudrate, timeout=None, **kwargs):
         self.port = port
